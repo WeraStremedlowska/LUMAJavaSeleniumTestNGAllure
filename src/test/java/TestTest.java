06dd173437cb687d9ef353f6fa1037731efd83f1
Wera Stremedlowska <stremedlowska@gmail.com>
2024-05-30 20:11:44 +0200

    Add allure report (#4)
    * Add allure report

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,8 +1,11 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
     @Test
-    public void test() {
+    public void testPass() {
+
+        Assert.assertTrue(true);
 
     }
 }
