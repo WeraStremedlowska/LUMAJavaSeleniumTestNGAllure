db22e012cc244dc75521ee005b9de21a85198e53
Wera Stremedlowska <Stremedlowska@gmail.com>
2024-06-09 11:09:07 +0200

    Add firefox options

--- src/test/java/com/lumatest/base/BaseUtils.java
+++ src/test/java/com/lumatest/base/BaseUtils.java
@@ -1,23 +1,22 @@
 package com.lumatest.base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.Properties;
 
 public final class BaseUtils {
     private static final String ENV_CHROME_OPTIONS = "CHROME_OPTIONS";
     private static final String ENV_BROWSER_OPTIONS = "BROWSER_OPTIONS";
-
     private static final String PREFIX_PROP = "default.";
     private static final String PROP_CHROME_OPTIONS = PREFIX_PROP + ENV_CHROME_OPTIONS.trim().toLowerCase();
 
@@ -35,15 +34,15 @@ public final class BaseUtils {
                     }
                 }
             } else {
-                try {
-                    InputStream inputStream = BaseUtils.class.getClassLoader().getResourceAsStream("local.properties");
+                try (InputStream inputStream = BaseUtils.class.getClassLoader().getResourceAsStream("local.properties")) {
                     if (inputStream == null) {
-                        System.out.println("ERROR: The \u001B[31mlocal.properties\u001B[0m file not found in src/test/resources/ directory.");
+                        System.out.println("ERROR: The local.properties file not found in src/test/resources/ directory.");
                         System.out.println("You need to create it from local.properties.TEMPLATE file.");
                         System.exit(1);
                     }
                     properties.load(inputStream);
-                } catch (IOException ignore) {
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
@@ -53,13 +52,18 @@ public final class BaseUtils {
         return System.getenv("CI_RUN") != null;
     }
 
-    private static final ChromeOptions chromeOptions;
+    private static final ChromeOptions chromeOptions = new ChromeOptions();
+    private static final FirefoxOptions firefoxOptions = new FirefoxOptions();
+    private static final SafariOptions safariOptions = new SafariOptions();
 
     static {
         initProperties();
+        setupChromeOptions();
+        setupFirefoxOptions();
+        setupSafariOptions();
+    }
 
-        chromeOptions = new ChromeOptions();
-
+    private static void setupChromeOptions() {
         String options = properties.getProperty(PROP_CHROME_OPTIONS);
         if (options != null) {
             for (String argument : options.split(";")) {
@@ -68,17 +72,31 @@ public final class BaseUtils {
         }
     }
 
+    private static void setupFirefoxOptions() {
+        firefoxOptions.addArguments("--incognito");
+        firefoxOptions.addArguments("--headless");
+        firefoxOptions.addArguments("--window-size=1920,1080");
+        firefoxOptions.addArguments("--disable-gpu");
+        firefoxOptions.addArguments("--no-sandbox");
+        firefoxOptions.addArguments("--disable-dev-shm-usage");
+        firefoxOptions.addArguments("--disable-web-security");
+        firefoxOptions.addArguments("--allow-running-insecure-content");
+        firefoxOptions.addArguments("--ignore-certificate-errors");
+    }
+
+    private static void setupSafariOptions() {
+        safariOptions.setAutomaticInspection(true);
+        safariOptions.setAutomaticProfiling(true);
+    }
+
     private static WebDriver createChromeDriver() {
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver(chromeOptions);
-        driver.executeCdpCommand("Network.enable", Map.of());
-        driver.executeCdpCommand("Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9")));
-        return driver;
+        return new ChromeDriver(chromeOptions);
     }
 
     private static WebDriver createFirefoxDriver() {
         WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver();
+        return new FirefoxDriver(firefoxOptions);
     }
 
     private static WebDriver createEdgeDriver() {
@@ -87,42 +105,18 @@ public final class BaseUtils {
     }
 
     private static WebDriver createSafariDriver() {
-        System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
-        System.setProperty("webdriver.safari.verboseLogging", "true");
-        SafariOptions safariOptions = new SafariOptions();
-        safariOptions.setAutomaticInspection(true);
-        safariOptions.setAutomaticProfiling(true);
-        try {
-            System.out.println("Setting up SafariDriver");
-            WebDriverManager.getInstance(SafariDriver.class).setup();
-            System.out.println("Creating SafariDriver instance");
-            WebDriver driver = new SafariDriver(safariOptions);
-            System.out.println("SafariDriver instance created successfully");
-            return driver;
-        } catch (Exception e) {
-            System.err.println("Error creating SafariDriver: " + e.getMessage());
-            e.printStackTrace();
-            throw e;
-        }
+        WebDriverManager.safaridriver().setup();
+        return new SafariDriver(safariOptions);
     }
-
-
 
     public static WebDriver createDriver(String driver) {
-        switch (driver) {
-            case "chrome" -> {
-                return createChromeDriver();
-            }
-            case "firefox" -> {
-                return createFirefoxDriver();
-            }
-            case "edge" -> {
-                return createEdgeDriver();
-            }
-            case "safari" -> {
-                return createSafariDriver();
-            }
+        return switch (driver) {
+            case "chrome" -> createChromeDriver();
+            case "firefox" -> createFirefoxDriver();
+            case "edge" -> createEdgeDriver();
+            case "safari" -> createSafariDriver();
             default -> throw new IllegalArgumentException("Unknown driver: " + driver);
-        }
+        };
     }
 }
+
