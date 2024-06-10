0848f6d2ff6bb60d4659531a62f4e9e8cc860924
Wera Stremedlowska <Stremedlowska@gmail.com>
2024-06-10 19:48:38 +0200

    Fix createDriver

--- src/test/java/com/lumatest/base/BaseUtils.java
+++ src/test/java/com/lumatest/base/BaseUtils.java
@@ -126,14 +126,14 @@ public final class BaseUtils {
 
     public static WebDriver createDriver(String driver) {
         String os = System.getProperty("os.name").toLowerCase();
-        if (driver.equals("safari") && !os.contains("mac")) {
+/*        if (driver.equals("safari") && !os.contains("mac")) {
             System.out.println("Safari is only supported on macOS. Skipping test.");
             return null;
         }
         if (driver.equals("edge") && !os.contains("win")) {
             System.out.println("Edge is only supported on Windows. Skipping test.");
             return null;
-        }
+        }*/
         return switch (driver) {
             case "chrome" -> createChromeDriver();
             case "firefox" -> createFirefoxDriver();
